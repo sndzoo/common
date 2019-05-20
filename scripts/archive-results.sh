7f376df1ddb3156c0b035b989b10496f1116eef5
peusterm <manuel.peuster@uni-paderborn.de>
2019-05-20 18:55:34 +0200

    Updated archiving script
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- scripts/archive-results.sh
+++ scripts/archive-results.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 set -e
-# collect inputs / conifgs
-TARGET=$1
-TARGET_DIR="../result-archive/$TARGET/"
-GIT_HASH=$(git rev-parse --short HEAD)
+# collect inputs / configs / results
+SOURCE=$1
+SOURCE_DIR="$SOURCE"
+TARGET=$2
+TARGET_DIR="$HOME/nfv-benchmarking-result-archive/$TARGET"
 
-echo "Archiving results/ to $TARGET_DIR ..."
+
+echo "Archiving tng-bench results..."
+echo "SOURCE_DIR=$SOURCE_DIR"
+echo "TARGET_DIR=$TARGET_DIR"
 
 # 0. make output dir
 mkdir $TARGET_DIR
 
 # 1. copy *.csv and YAML files
-cp results/*.csv $TARGET_DIR
-cp results/*.yml $TARGET_DIR
+cp $SOURCE_DIR/*.csv $TARGET_DIR
+cp $SOURCE_DIR/*.yml $TARGET_DIR
 
 # 2. tar all raw results to target dir
-tar -zcf $TARGET_DIR/raw_results.tar.gz results/
+tar -zcf $TARGET_DIR/raw_results.tar.gz $SOURCE_DIR
 
 # 3.
-tar -zcf $TARGET_DIR/prometheus-data.tar.gz ~/tng-sdk-benchmark/prometheus/prometheus-data/
-
-# 4. store git hash
-echo $GIT_HASH > $TARGET_DIR/githash.txt
+tar -zcf $TARGET_DIR/prometheus-data.tar.gz $HOME/tng-sdk-benchmark/prometheus/prometheus-data/
 
 # finally print the outputs
-tree -h $TARGET_DIR
 echo "----------------------------------"
-echo "Git hash for logbook: $GIT_HASH"
+tree -h $TARGET_DIR
 echo "----------------------------------"
 echo "Done."
